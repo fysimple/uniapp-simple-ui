f32a5b32865c17d4d819d88973b69cca59a3ce8a
fy simple <1548276041@qq.com>
2020-08-25 18:04:12 +0800

    问题界面

--- pages-question/index/index.vue
+++ pages-question/index/index.vue
@@ -1,11 +1,41 @@
 <template>
 	<view>
 		<view class="com-title box-mg-spacing">问题分类</view>
-		<view class="font-size-sm text-color-grey box-mg-spacing">前端问题：主要包括HTML（标签语义化、H5标签等）、CSS（新属性的使用、定位、浮动、清除浮动、动画效果以及各个浏览器的兼容问题）、
-		JAVASCRIPT(语法、ES6、WEB API)、VUE（...）、REACT（...），后期会添加一些小程序的常用框架...未完待续...</view>
+		<view class="font-size-sm text-color-grey page-sub-title">VUE、路由、VUEX、面试题</view>
 		<view class="list-wrapper">
-			<view class="com-box-spacing"></view>
+			<view class="flex flex_col3">
+				<view class="flex-item">
+					<view class="cont">VUE</view>
+				</view>
+				<view class="flex-item">
+					<view class="cont">V路由</view>
+				</view>
+				<view class="flex-item">
+					<view class="cont">VUEX</view>
+				</view>
+				<view class="flex-item">
+					<view class="cont">V面试题</view>
+				</view>
+			</view>
 		</view>
+		<view class="font-size-sm text-color-grey page-sub-title">HTML的属性、标签、进阶、面试题</view>
+		<view class="list-wrapper">
+			<view class="flex flex_col3">
+				<view class="flex-item">
+					<view class="cont">H属性</view>
+				</view>
+				<view class="flex-item">
+					<view class="cont">H标签</view>
+				</view>
+				<view class="flex-item">
+					<view class="cont">H进阶</view>
+				</view>
+				<view class="flex-item">
+					<view class="cont">H面试题</view>
+				</view>
+			</view>
+		</view>
+		
 	</view>
 </template>
 
@@ -25,5 +55,22 @@
 </script>
 
 <style lang="scss">
-	
+	.page-sub-title{
+		margin: auto $uni-spacing-col-lg $uni-spacing-col-lg;
+	}
+	.list-wrapper{
+		margin: auto $uni-spacing-col-lg - $uni-spacing-col-base auto $uni-spacing-col-lg;
+	}
+	.flex-item{
+		padding-right: $uni-spacing-col-base;
+		.cont{
+			font-size: $uni-font-size-sm;
+			font-weight: 500;
+			line-height: 100rpx;
+			text-align: center;
+			background-color: rgba($color: $uni-color-primary, $alpha: .1);
+			box-shadow: $box-shadow;
+			border-radius: 12rpx;
+		}
+	}
 </style>
